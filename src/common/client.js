0f6571bd3eb5a1cc838962cb276f3a427da56d6a
Randa <82415612+randa-11295@users.noreply.github.com>
2022-06-04 12:52:30 +0200

    Update client.js

--- src/common/client.js
+++ src/common/client.js
@@ -1,8 +1,8 @@
 import {  ApolloClient,  InMemoryCache} from "@apollo/client";
 
 export const client = new ApolloClient({
-     uri: 'http://localhost:4000/',
-     //  'https://randa-shop-api.herokuapp.com/' ,
+     uri: 'https://randa-shop-api.herokuapp.com/',
+   
      cache: new InMemoryCache()
 });
    
